6196f53276cb8c91372300b698b443a925a75593
Anirudh Mathukumilli <anirudh.mathukumilli@gmail.com>
2018-03-04 03:21:29 -0800

    Wrote skeleton for CrowdEmotion object.
    Filled in some of the declarations for methods
    the CrowdEmotion class will need.

--- crowdEmotion.py
+++ crowdEmotion.py
@@ -6,9 +6,18 @@ import argparse
 
 class CrowdEmotion:
   
+  # returns the current emotional state of the crowd
+  def getCurrentEmotion(self):
+    return self.emotion
+
+  # takes a filepath to an image, calls CF API on the
+  # image, and then sets the emotional state 
+  def processEmotion(self, filename):
+    return
 
   def __init__(self):
     self.key = '51fae3a010d1498d95a008972adb3547'
     # set api key
     CF.Key.set(self.key) 
-    
+    # initialize the emotion object
+    self.emotion = None
